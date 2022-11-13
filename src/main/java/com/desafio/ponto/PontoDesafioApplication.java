cc10ea82daeedc20233929e59e865d675cb91bfa
Gabriel Oliveira <gabriel.oliveira@paytrack.com.br>
2022-11-13 18:50:05 -0300

    Commit inicial.

---
+++ src/main/java/com/desafio/ponto/PontoDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.ponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/ponto/PontoDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.ponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
